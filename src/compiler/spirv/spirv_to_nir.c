909225d0e464a14b21f2f3a41d84f4124b584872
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2018-03-09 10:27:20 +0100

    nir/spirv: add support for the SubgroupVoteKHR SPIR-V capability
    This capability is required for the VK_EXT_shader_subgroup_vote extension.

--- src/compiler/spirv/spirv_to_nir.c
+++ src/compiler/spirv/spirv_to_nir.c
@@ -3378,6 +3378,7 @@ vtn_handle_preamble_instruction(struct vtn_builder *b, SpvOp opcode,
          spv_check_supported(subgroup_basic, cap);
          break;
 
+      case SpvCapabilitySubgroupVoteKHR:
       case SpvCapabilityGroupNonUniformVote:
          spv_check_supported(subgroup_vote, cap);
          break;
@@ -4023,6 +4024,9 @@ vtn_handle_body_instruction(struct vtn_builder *b, SpvOp opcode,
    case SpvOpGroupNonUniformLogicalXor:
    case SpvOpGroupNonUniformQuadBroadcast:
    case SpvOpGroupNonUniformQuadSwap:
+   case SpvOpSubgroupAllKHR:
+   case SpvOpSubgroupAnyKHR:
+   case SpvOpSubgroupAllEqualKHR:
       vtn_handle_subgroup(b, opcode, w, count);
       break;
 
