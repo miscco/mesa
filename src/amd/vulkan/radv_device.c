fa886cb8842c8af6227532dc9a5b3d1a3305efdd
Daniel Schürmann <daniel.schuermann@campus.tu-berlin.de>
2019-04-01 11:36:32 +0200

    radv: introduce new RADV_PERFTEST_ACO option and enable by default

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -481,6 +481,7 @@ static const struct debug_control radv_perftest_options[] = {
 	{"localbos", RADV_PERFTEST_LOCAL_BOS},
 	{"dccmsaa", RADV_PERFTEST_DCC_MSAA},
 	{"bolist", RADV_PERFTEST_BO_LIST},
+	{"aco", RADV_PERFTEST_ACO},
 	{NULL, 0}
 };
 
@@ -576,7 +577,8 @@ VkResult radv_CreateInstance(
 
 	instance->perftest_flags = parse_debug_string(getenv("RADV_PERFTEST"),
 						   radv_perftest_options);
-
+	#warning "ACO enabled by default"
+	instance->perftest_flags |= RADV_PERFTEST_ACO;
 
 	if (instance->debug_flags & RADV_DEBUG_STARTUP)
 		radv_logi("Created an instance");
