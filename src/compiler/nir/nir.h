da02feeb2e91d663aae1304253e62d07773e0888
Rhys Perry <pendingchaos02@gmail.com>
2019-05-22 19:07:50 +0100

    nir: combine move_load_const and opt_sink

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -3277,7 +3277,6 @@ bool nir_remove_dead_variables(nir_shader *shader, nir_variable_mode modes);
 bool nir_lower_constant_initializers(nir_shader *shader,
                                      nir_variable_mode modes);
 
-bool nir_move_load_const(nir_shader *shader);
 bool nir_move_vec_src_uses_to_dest(nir_shader *shader);
 bool nir_lower_vec_to_movs(nir_shader *shader);
 void nir_lower_alpha_test(nir_shader *shader, enum compare_func func,
@@ -3659,7 +3658,7 @@ bool nir_opt_remove_phis(nir_shader *shader);
 
 bool nir_opt_shrink_load(nir_shader *shader);
 
-bool nir_opt_sink(nir_shader *shader);
+bool nir_opt_sink(nir_shader *shader, bool sink_intrinsics);
 
 bool nir_opt_trivial_continues(nir_shader *shader);
 
