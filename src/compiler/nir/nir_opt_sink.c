ff0e48b60afc4d47b2d3138a1358d4045eb1f705
Rhys Perry <pendingchaos02@gmail.com>
2019-05-22 19:07:50 +0100

    nir: combine move_load_const and opt_sink

--- src/compiler/nir/nir_opt_sink.c
+++ src/compiler/nir/nir_opt_sink.c
@@ -1,5 +1,6 @@
 /*
- * Copyright © 2018 Valve Corporation
+ * Copyright © 2018 Red Hat
+ * Copyright © 2019 Valve Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -20,25 +21,66 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  *
+ * Authors:
+ *    Rob Clark (robdclark@gmail.com>
+ *    Daniel Schürmann (daniel.schuermann@campus.tu-berlin.de)
+ *    Rhys Perry (pendingchaos02@gmail.com)
+ *
  */
 
 #include "nir.h"
 
+
+/*
+ * A simple pass that moves some instructions into the least common
+ * anscestor of consuming instructions.
+ */
+
+static nir_loop *
+get_loop(nir_block *block)
+{
+   nir_cf_node *node = &block->cf_node;
+   while (node) {
+      if (node->type == nir_cf_node_loop)
+         return (nir_loop*)node;
+      node = node->parent;
+   }
+   return NULL;
+}
+
+static bool
+inside_loop(nir_block *block, nir_loop *loop)
+{
+   if (!loop)
+      return true;
+
+   nir_cf_node *node = &block->cf_node;
+   while (node) {
+      if (node->type == nir_cf_node_loop && (nir_loop*)node == loop)
+         return true;
+      node = node->parent;
+   }
+   return false;
+}
+
+/* iterate a ssa def's use's and try to find a more optimal block to
+ * move it to, using the dominance tree.  In short, if all of the uses
+ * are contained in a single block, the load will be moved there,
+ * otherwise it will be move to the least common ancestor block of all
+ * the uses
+ */
 static nir_block *
-find_lca(nir_ssa_def *def)
+get_preferred_block(nir_ssa_def *def, bool loop_aware)
 {
    nir_block *lca = NULL;
-   nir_block *def_block = def->parent_instr->block;
 
    /* hmm, probably ignore if-uses: */
    if (!list_empty(&def->if_uses))
-      return def_block;
+      return NULL;
 
    nir_foreach_use(use, def) {
       nir_instr *instr = use->parent_instr;
       nir_block *use_block = instr->block;
-      if (use_block == def_block)
-         return def_block;
 
       /*
        * Kind of an ugly special-case, but phi instructions
@@ -49,11 +91,19 @@ find_lca(nir_ssa_def *def)
        */
       if (instr->type == nir_instr_type_phi) {
          nir_phi_instr *phi = nir_instr_as_phi(instr);
+         nir_block *phi_lca = NULL;
          nir_foreach_phi_src(src, phi) {
-            if (&src->src == use) {
-               use_block = src->pred;
-               break;
-            }
+            if (&src->src == use)
+               phi_lca = nir_dominance_lca(phi_lca, src->pred);
+         }
+         use_block = phi_lca;
+      }
+
+      if (loop_aware) {
+         nir_loop *use_loop = get_loop(use_block);
+         if (!inside_loop(def->parent_instr->block, use_loop)) {
+            use_block = nir_block_cf_tree_prev(nir_loop_first_block(use_loop));
+            assert(nir_block_dominates(def->parent_instr->block, use_block));
          }
       }
 
@@ -83,95 +133,61 @@ insert_after_phi(nir_instr *instr, nir_block *block)
    exec_list_push_tail(&block->instr_list, &instr->node);
 }
 
-static bool
-nir_opt_sink_block(nir_block *block)
-{
-   bool progress = false;
-
-   nir_foreach_instr_reverse_safe(instr, block) {
-      nir_ssa_def *def;
-
-      switch (instr->type) {
-      case nir_instr_type_phi:
-      case nir_instr_type_call:
-      case nir_instr_type_jump:
-      case nir_instr_type_parallel_copy:
-      case nir_instr_type_alu:
-         continue;
-      case nir_instr_type_ssa_undef: {
-         nir_ssa_undef_instr *undef = nir_instr_as_ssa_undef(instr);
-         def = &undef->def;
-         break;
-      }
-      case nir_instr_type_load_const: {
-         nir_load_const_instr *load_const = nir_instr_as_load_const(instr);
-         def = &load_const->def;
-         break;
-      }
-      case nir_instr_type_tex:
-         //TODO
-         continue;
-      case nir_instr_type_intrinsic: {
-         nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
-         if (!nir_intrinsic_infos[intrin->intrinsic].has_dest)
-            continue;
-         def = &intrin->dest.ssa;
-         if (intrin->intrinsic == nir_intrinsic_load_interpolated_input ||
-             intrin->intrinsic == nir_intrinsic_load_ubo)
-            break;
-         //TODO some can directly go on, some need beneficial test
-         continue;
-      }
-      case nir_instr_type_deref:
-         continue;
-      default:
-         nir_print_instr (instr, stderr);
-         unreachable("");
-      }
-
-      nir_block *lca = find_lca(def);
-      // TODO: check that lca is not in a loop when original block isn't
-      if (lca == instr->block) {
-         continue;
-      }
-
-      exec_node_remove(&instr->node);
-      insert_after_phi(instr, lca);
-      instr->block = lca;
-
-      progress = true;
+static nir_ssa_def *
+get_move_def(nir_instr *instr, bool sink_intrinsics) {
+   if (instr->type == nir_instr_type_load_const) {
+      return &nir_instr_as_load_const(instr)->def;
+   } else if (instr->type == nir_instr_type_intrinsic && sink_intrinsics) {
+      nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+      return intrin->intrinsic == nir_intrinsic_load_interpolated_input ||
+             intrin->intrinsic == nir_intrinsic_load_ubo ? &intrin->dest.ssa : NULL;
+   } else if (instr->type == nir_instr_type_ssa_undef) {
+      return &nir_instr_as_ssa_undef(instr)->def;
+   } else {
+      return NULL;
    }
-
-   return progress;
-}
-
-static bool
-nir_opt_sink_impl(nir_function_impl *impl)
-{
-   nir_metadata_require(impl,
-                        nir_metadata_dominance |
-                        nir_metadata_block_index | nir_metadata_loop_analysis);
-   bool progress = false;
-
-   nir_foreach_block_reverse(block, impl)
-      progress |= nir_opt_sink_block(block);
-
-   nir_metadata_preserve(impl,
-                         nir_metadata_dominance |
-                         nir_metadata_block_index | nir_metadata_loop_analysis);
-   return progress;
 }
 
 bool
-nir_opt_sink(nir_shader *shader)
+nir_opt_sink(nir_shader *shader, bool sink_intrinsics)
 {
    bool progress = false;
 
    nir_foreach_function(function, shader) {
-      if (function->impl)
-         progress |= nir_opt_sink_impl(function->impl);
+      if (!function->impl)
+         continue;
+
+      nir_metadata_require(function->impl,
+                           nir_metadata_block_index | nir_metadata_dominance);
+
+      nir_foreach_block_reverse(block, function->impl) {
+         nir_foreach_instr_reverse_safe(instr, block) {
+            nir_ssa_def *def = get_move_def(instr, sink_intrinsics);
+            if (!def)
+               continue;
+
+            nir_block *use_block =
+                  get_preferred_block(def, instr->type == nir_instr_type_intrinsic);
+
+            if (!use_block)
+               continue;
+
+            if (use_block == instr->block)
+               continue;
+
+            exec_node_remove(&instr->node);
+
+            insert_after_phi(instr, use_block);
+
+            instr->block = use_block;
+
+            progress = true;
+         }
+      }
+
+      nir_metadata_preserve(function->impl,
+                            nir_metadata_block_index | nir_metadata_dominance);
    }
 
    return progress;
 }
-
