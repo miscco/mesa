8788feaecd74ac3f9940df6893bc5b75f37b2ede
Rhys Perry <pendingchaos02@gmail.com>
2019-06-27 11:29:33 +0100

    radv: add RADV_PERFTEST=llvm

--- src/amd/vulkan/radv_device.c
+++ src/amd/vulkan/radv_device.c
@@ -490,6 +490,7 @@ static const struct debug_control radv_perftest_options[] = {
 	{"shader_ballot", RADV_PERFTEST_SHADER_BALLOT},
 	{"tccompatcmask", RADV_PERFTEST_TC_COMPAT_CMASK},
 	{"aco", RADV_PERFTEST_ACO},
+	{"llvm", RADV_PERFTEST_LLVM},
 	{NULL, 0}
 };
 
@@ -592,8 +593,14 @@ VkResult radv_CreateInstance(
 
 	instance->perftest_flags = parse_debug_string(getenv("RADV_PERFTEST"),
 						   radv_perftest_options);
-	#warning "ACO enabled by default"
-	instance->perftest_flags |= RADV_PERFTEST_ACO;
+
+	if (instance->perftest_flags & RADV_PERFTEST_ACO) {
+		instance->perftest_flags &= ~RADV_PERFTEST_LLVM;
+	} else if (!(instance->perftest_flags & RADV_PERFTEST_LLVM)) {
+		#warning "ACO enabled by default"
+		instance->perftest_flags |= RADV_PERFTEST_ACO;
+	}
+
 	if (instance->perftest_flags & RADV_PERFTEST_ACO)
 		fprintf(stderr, "WARNING: Experimental compiler backend enabled. Here be dragons! Incorrect rendering, GPU hangs and/or resets are likely\n");
 
